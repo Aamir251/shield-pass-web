1556e5e4181b4a343ac7f6338eeec64a283b5593
Aamir251 <ecodeshub@gmail.com>
2024-08-28 21:52:34 +0530

    temp page minor change

--- src/app/temp/page.tsx
+++ src/app/temp/page.tsx
@@ -20,9 +20,9 @@ const TempPage = () => {
               Password
             </label>
             <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-200 mb-3 leading-tight focus:outline-none focus:shadow-outline"
               id="password"
-              type="password"
+              type="text"
               placeholder="Enter your password"
             />
           </div>

1b2fae53ef4b1c4631f0f1420158d5cb04cf19b1
Aamir251 <ecodeshub@gmail.com>
2024-09-02 17:25:13 +0530

    constant base url changed

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,5 @@
 export const CREDENTIAL_CATEGORIES = ["logins", "websites", "apps", "socials"];
 
-export const BASE_URL = "/dashboard/personal/credentials/logins";
+export const BASE_URL = "/personal/credentials/logins";
 
 export const CREDENTIAL_TYPES = ["personal", "work", "business"];
